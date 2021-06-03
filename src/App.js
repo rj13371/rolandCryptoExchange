db918b57c37bcff4f952c1a4cd07a83daac5032b
rj13371 <mathesonroland@gmail.com>
2021-06-03 20:26:02 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Coin from './components/Coin/Coin'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Roland Exchange</h1>
+
+      </header>
+      <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Ticker</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+      <Coin name = 'Bitcoin' ticker = 'BTC' price = {100000}/>
+      <Coin name = 'Ethereum' ticker = 'ETH' price = {99}/>
+      <Coin name = 'Tether' ticker = 'USDT'price = {1}/>
+      <Coin name = 'Ripple' ticker = 'XRP'price = {9}/>
+      </tbody>
+      </table>
+      
+    </div>
+  );
+}
+
+export default App;
